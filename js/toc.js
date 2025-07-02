e3b25b5d04aaf473de7399fb4d2f796a7769cf68
devashish <devashish.86@gmail.com>
2025-07-01 21:23:47 -0700

    updating copyright

--- js/toc.js
+++ js/toc.js
@@ -1,55 +1,45 @@
 var spy = function () {
-    var elems = $(":header");
-    if (elems.length == 0) {
-        return;
-    }
-    var currentTop = $(window).scrollTop();
-    var currentBottom = $(window).scrollTop() + $(window).height();
-    var pageBottom = $('#EOF').offset().top;
-
-    var meetUnread = false
-    var currentIndex = -1
-    elems.each(function (idx) {
-        var elemTop = $(this).offset().top;
-        var id = $(this).attr('id');
-        var navElem = $('#' + id + '-nav');
-        if (currentTop + $(this).height() >= elemTop || currentBottom >= pageBottom) {
-            navElem.addClass('toc-active');
-        } else {
-            if (meetUnread == false) {
-                meetUnread = true
-                currentIndex = idx - 1
-            }
-            navElem.removeClass('toc-active');
-        }
-    })
-}
-$().ready(function () {
-    $(".collapse").each(function (idx) {
-        $(this).collapse("show");
-    });
-    spy();
-    $(window).bind('scroll', throttle(spy));
-});
-
-function throttle(func, timeout = 250) {
-    let last;
-    let timer;
-   
-    return function () {
-      const context = this;
-      const args = arguments;
-      const now = +new Date();
-   
-      if (last && now < last + timeout) {
-        clearTimeout(timer)
-        timer = setTimeout(function () {
-          last = now
-          func.apply(context, args)
-        }, timeout)
-      } else {
-        last = now
-        func.apply(context, args)
-      }
-    }
+  var elems = document.querySelectorAll(Array.from(Array(6).keys(), x => ".post-body h"+(x+1).toString()));
+  // ":is()" was not supported until Chrome 88+
+  // Here is a backfill
+  if (elems.length == 0) {
+    return;
   }
+  var supportPageOffset = window.pageXOffset !== undefined;
+  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
+  var currentTop = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+  var currentBottom = currentTop + window.height;
+  var pageBottom = window.pageBottom;
+
+  var meetUnread = false
+  let lastElemName = elems[elems.length - 1].id;
+  elems.forEach(function (elem, idx) {
+    var elemTop = elem.offsetTop;
+    var id = elem.getAttribute('id');
+    var navElems = document.getElementsByClassName("nav-"+id);
+    if (navElems.length == 0) {
+      return
+    }
+    if (currentTop >= elemTop || currentBottom >= pageBottom) {
+      Array.from(navElems).forEach((e) => {
+        e.classList.add('toc-active');
+      });
+    } else {
+      if (meetUnread == false) {
+        meetUnread = true;
+        if (idx > 0) {
+          lastElemName = elems[idx - 1].id; 
+        }
+      }
+      Array.from(navElems).forEach((e) => {
+        e.classList.remove('toc-active');
+      });
+    }
+  })
+  let selector = ".nav-" + lastElemName;
+  // Two toc elements here
+  document.querySelectorAll(selector).forEach(e => {
+    e.scrollIntoView({ block: "center", behavior: 'smooth' });
+  });
+}
